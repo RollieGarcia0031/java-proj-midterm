4927b9b6fe763b5437aeb0df49f3ea16bcd04049
RollieGarcia0031 <garciarollie827@gmail.com>
2025-10-22 07:33:37 +0800

    add new logo for scopes

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args){
         engineer();
         System.out.print("\n\n\n\n");
-        logo2();
+        // logo2();
     }
 
     public static void engineer(){        
