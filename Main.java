37a9a4193d43473ff1823e77b8061cc84b831011
RollieGarcia0031 <garciarollie827@gmail.com>
2025-10-22 17:21:25 +0800

    created pattern based on circle general formula

--- Main.java
+++ Main.java
@@ -1,197 +1,305 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         engineer();
         System.out.print("\n\n\n\n");
         // logo2();
+
+        // int number = 2;
+
+        // double guess = number / 2; // Initial guess
+        // double epsilon = 0.000001; // Precision threshold
+
+        // // Iterate until the difference between guesses is small
+        // while ((guess * guess - number) > epsilon || (number - guess * guess) >
+        // epsilon) {
+        // guess = (guess + number / guess) / 2;
+        // }
+
+        // System.out.println(guess);
+
     }
 
-    public static void engineer(){        
-        System.out.println("          _______");
+    public static void engineer() {
+        // the space between logo and left side of screen
+        int marginLeft = 40;
+        // length of the logo container
+        int logoLength = 80 - 41;
+        // the string that will be used as vertical border
+        String verticalLine = "I";
+        int verticalLineStrLength = 1; // string length/amount of char of verticalLine
+        int verticalLineHeight = 21 - 7; // height of the verticalLine height
+        // the string that will be used as top border
+        String topLine = "=";
+        // margin between the vertical border and circle inside logo
+        int innerMarginX = 3;
+        // the radius of the main circle in logo
+        int iconRadius = 5;
 
-        int bodyWidth = 6;
-        for (int row = 9; row >= 4; row --){
-            int space = 1;
-            while (space <= row){
+        // top border
+        for (int i = 1; i <= marginLeft; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= logoLength; i++)
+            System.out.print(topLine);
+        System.out.println();
+
+        // blank layer
+        for (int repeat = 1; repeat <= 2; repeat++) {
+            for (int i = 1; i <= marginLeft; i++)
                 System.out.print(" ");
-                space ++;
-            }
-
-            if (row >= 6){
-                System.out.print("/");
-                for (int i = 1; i <= bodyWidth; i ++) System.out.print(" ");
-                System.out.print("/");
-            }
-            if (row == 6) {
-                for(int i = 1; i <= bodyWidth/2+1; i ++)System.out.print('_'); 
-            }
-            if (row <= 5) {
-                System.out.print("/");
-                for (int i = 1;i <= 6*2-1; i ++)System.out.print(" ");
-                System.out.print("/");
-            }
-
+            System.out.print(verticalLine);
+            for (int i = 1; i <= logoLength - (verticalLineStrLength * 2); i++)
+                System.out.print(" ");
+            System.out.print(verticalLine);
             System.out.println();
         }
 
-        System.out.println("   /_____      /");      
+        for (int y = 1; y <= verticalLineHeight; y++) {
+            // print the margins between the screen and logo
+            for (int i = 1; i <= marginLeft; i++)
+                System.out.print(" ");
 
-        for (int column = 8; column >= 4; column --){
-            for (int space = 1; space <= column; space ++) System.out.print(" ");
+            // print the vertical border
+            System.out.print(verticalLine);
 
-            if (column > 4) System.out.println("/     /");
-            else System.out.println("/_____/");
+            // print the margin between the logo and the verical border
+            for (int i = 1; i <= innerMarginX; i++)
+                System.out.print(" ");
+
+            /*
+             * this part calculates the square root
+             */
+            // the x coordinate
+            int xSqaured = (iconRadius * iconRadius) - ((y - 7) * (y - 7));
+            double spaceX = xSqaured / 2; // Initial guess
+            double epsilon = 0.000001; // Precision threshold
+
+            if (xSqaured >= 0) {
+
+                // Iterate until the difference between guesses is small
+                while ((spaceX * spaceX - xSqaured) > epsilon || (xSqaured - spaceX * spaceX) > epsilon) {
+                    spaceX = (spaceX + xSqaured / spaceX) / 2;
+                }
+            }
+            spaceX += 2;
+            // END OF SQUARE ROOT
+
+            for (int i = 1; i <= spaceX; i++)
+                System.out.print(" ");
+
+            System.out.print("@");
+            System.out.println(verticalLine);
         }
+        System.out.print("done");
+
     }
 
-    public static void logo2(){
-        
-        //layer 1
-        for (int i = 1; i <= 34; i ++)System.out.print(" ");
-        for (int i = 1; i <= 30; i ++)System.out.print("_");
+    public static void logo2() {
+
+        // layer 1
+        for (int i = 1; i <= 34; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 30; i++)
+            System.out.print("_");
 
         System.out.println();
 
-        //layer 2
-        for (int i = 0; i <= 24;i ++) System.out.print(" ");
-        for (int i = 1; i<= 7; i ++) System.out.print("@");
+        // layer 2
+        for (int i = 0; i <= 24; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 7; i++)
+            System.out.print("@");
         System.out.print("  |");
-        for (int i = 1; i <= 28; i ++) System.out.print(' ');
+        for (int i = 1; i <= 28; i++)
+            System.out.print(' ');
 
         System.out.println("|");
 
-        //layer 3,4
-        for (int k = 1; k <= 2; k ++ ){
-            for(int i = 1; i <= 26; i++) System.out.print(" ");
-            for(int i = 1; i <= 6; i ++) System.out.print("@");
+        // layer 3,4
+        for (int k = 1; k <= 2; k++) {
+            for (int i = 1; i <= 26; i++)
+                System.out.print(" ");
+            for (int i = 1; i <= 6; i++)
+                System.out.print("@");
             System.out.print("  |");
-            for(int i = 1; i <= 28; i ++) System.out.print(" ");
+            for (int i = 1; i <= 28; i++)
+                System.out.print(" ");
 
             System.out.println("|");
         }
 
-        //layer 5
-        for (int i = 1; i <= 18; i ++) System.out.print(" ");
-        for (int i = 1; i <= 14; i++) System.out.print("@");
+        // layer 5
+        for (int i = 1; i <= 18; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 14; i++)
+            System.out.print("@");
         System.out.print("  |");
-        for (int i = 1; i <= (11); i ++) System.out.print(" ");
-        for (int i = 1; i <= 17; i++) System.out.print("_");
+        for (int i = 1; i <= (11); i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 17; i++)
+            System.out.print("_");
         System.out.println("|");
 
         int letterBaseWidth = 10;
 
         // layer 6,7
-        for (int repeat = 1; repeat <= 2; repeat ++){
-            for (int i = 1; i <= 16; i++) System.out.print(" ");
+        for (int repeat = 1; repeat <= 2; repeat++) {
+            for (int i = 1; i <= 16; i++)
+                System.out.print(" ");
             if (repeat == 2) {
-                for (int i = 1; i <= 12; i ++) System.out.print("@");
+                for (int i = 1; i <= 12; i++)
+                    System.out.print("@");
                 System.out.print("      |");
             } else {
-                for (int i = 1; i <= 34-18; i ++) System.out.print("@");
+                for (int i = 1; i <= 34 - 18; i++)
+                    System.out.print("@");
                 System.out.print("  |");
             }
-            for (int i = 1; i <= letterBaseWidth; i++) System.out.print(" ");
+            for (int i = 1; i <= letterBaseWidth; i++)
+                System.out.print(" ");
             System.out.println("|");
         }
 
-        int middleLetterLength = 61-36;
+        int middleLetterLength = 61 - 36;
 
-        //layer 8
-        for (int i = 1; i <= 19; i ++) System.out.print(" ");
-        for (int i = 1; i <= 8; i ++) System.out.print("@");
+        // layer 8
+        for (int i = 1; i <= 19; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 8; i++)
+            System.out.print("@");
         System.out.print("       |");
-        for (int i = 1; i <= letterBaseWidth; i ++) System.out.print(" ");
+        for (int i = 1; i <= letterBaseWidth; i++)
+            System.out.print(" ");
         System.out.print("|");
-        for (int i = 1; i <= middleLetterLength - letterBaseWidth; i ++) System.out.print("_");
+        for (int i = 1; i <= middleLetterLength - letterBaseWidth; i++)
+            System.out.print("_");
         System.out.println();
 
         // layer 9
-        for (int i = 1; i <= 18; i ++) System.out.print(" ");
-        for (int i = 1; i <= 8; i ++) System.out.print("@");
+        for (int i = 1; i <= 18; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 8; i++)
+            System.out.print("@");
         System.out.print("        |");
-        for (int i = 1; i <= middleLetterLength; i ++) System.out.print(" ");
+        for (int i = 1; i <= middleLetterLength; i++)
+            System.out.print(" ");
         System.out.println("|");
-        
+
         // layer 10,11,12
-        for (int repeat = 1; repeat <= 3; repeat ++){
-            for (int i = 1; i <= 14; i++) System.out.print(" ");
-            for (int i = 1; i <= 11; i++) System.out.print("@");
-            for (int i = 1; i <= 9; i++) System.out.print(" ");
+        for (int repeat = 1; repeat <= 3; repeat++) {
+            for (int i = 1; i <= 14; i++)
+                System.out.print(" ");
+            for (int i = 1; i <= 11; i++)
+                System.out.print("@");
+            for (int i = 1; i <= 9; i++)
+                System.out.print(" ");
             System.out.print("|");
-            if (repeat == 3){
-                for (int i = 1; i <= letterBaseWidth; i ++) System.out.print(" ");
-                for (int i = 1; i <= middleLetterLength - letterBaseWidth; i ++) System.out.print("_");
+            if (repeat == 3) {
+                for (int i = 1; i <= letterBaseWidth; i++)
+                    System.out.print(" ");
+                for (int i = 1; i <= middleLetterLength - letterBaseWidth; i++)
+                    System.out.print("_");
             } else {
-                for (int i = 1; i <= middleLetterLength; i ++) System.out.print(" ");
+                for (int i = 1; i <= middleLetterLength; i++)
+                    System.out.print(" ");
             }
             System.out.println("|");
         }
 
         // layer 13
-        for (int i = 1; i <= 18; i++) System.out.print(" ");
-        for (int i = 1; i <= 8; i ++) System.out.print("@");
-        for (int i = 1; i <=8; i ++) System.out.print(" ");
+        for (int i = 1; i <= 18; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 8; i++)
+            System.out.print("@");
+        for (int i = 1; i <= 8; i++)
+            System.out.print(" ");
         System.out.print("|");
-        for (int i = 1; i <= letterBaseWidth; i ++) System.out.print(" ");
+        for (int i = 1; i <= letterBaseWidth; i++)
+            System.out.print(" ");
         System.out.println("|");
 
-        //layer 14
-        for (int i = 1; i <= 18; i++) System.out.print(" ");
-        for (int i = 1; i <= 9; i ++) System.out.print("@");
-        for (int i = 1; i <=7; i ++) System.out.print(" ");
+        // layer 14
+        for (int i = 1; i <= 18; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 9; i++)
+            System.out.print("@");
+        for (int i = 1; i <= 7; i++)
+            System.out.print(" ");
         System.out.print("|");
-        for (int i = 1; i <= letterBaseWidth; i ++) System.out.print(" ");
+        for (int i = 1; i <= letterBaseWidth; i++)
+            System.out.print(" ");
         System.out.println("|");
 
-        int bottomLineLength = 65 - 36; 
+        int bottomLineLength = 65 - 36;
 
-        //layer 15
-        for (int i = 1; i <= 19; i ++) System.out.print(" ");
-        for (int i = 1; i <= 9; i ++) System.out.print("@");
-        for (int i = 1; i <= 6; i ++) System.out.print(" ");
+        // layer 15
+        for (int i = 1; i <= 19; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 9; i++)
+            System.out.print("@");
+        for (int i = 1; i <= 6; i++)
+            System.out.print(" ");
         System.out.print("|");
-        for (int i = 1; i <= letterBaseWidth; i ++) System.out.print(" ");
+        for (int i = 1; i <= letterBaseWidth; i++)
+            System.out.print(" ");
         System.out.print("|");
-        for (int i = 1; i <= bottomLineLength - letterBaseWidth; i ++)System.out.print("_");
+        for (int i = 1; i <= bottomLineLength - letterBaseWidth; i++)
+            System.out.print("_");
         System.out.println();
 
-        //layer 16
-        for (int i = 1; i <= 17; i++) System.out.print(" ");
-        for (int i = 1; i <= 13; i ++) System.out.print("@");
-        for (int i = 1; i <= 4; i++) System.out.print(" ");
+        // layer 16
+        for (int i = 1; i <= 17; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 13; i++)
+            System.out.print("@");
+        for (int i = 1; i <= 4; i++)
+            System.out.print(" ");
         System.out.print("|");
-        for (int i = 1; i<= bottomLineLength; i++) System.out.print(" ");
-        System.out.println("|"); 
+        for (int i = 1; i <= bottomLineLength; i++)
+            System.out.print(" ");
+        System.out.println("|");
 
         // layer 17
-        for (int i = 1; i <= 22; i ++) System.out.print(" ");
-        for (int i = 1; i <= 9; i ++) System.out.print("@");
-        for (int i = 1; i <= 3; i ++) System.out.print(" ");
+        for (int i = 1; i <= 22; i++)
+            System.out.print(" ");
+        for (int i = 1; i <= 9; i++)
+            System.out.print("@");
+        for (int i = 1; i <= 3; i++)
+            System.out.print(" ");
         System.out.print("|");
-        for (int i = 1; i <= bottomLineLength; i ++) System.out.print(" ");
+        for (int i = 1; i <= bottomLineLength; i++)
+            System.out.print(" ");
         System.out.println("|");
     }
 }
 
-// .......  ...... ........       .................................................... . ... .   ..
-// ............................:.. ............................... ...  .  ......... .
-// . .... ................#@@@@@*..@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:  .     ...  . ...   ..
-// ..... ................ *@@@@@@-.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=           ...   ....
-// ...................  ..*@@@@@@-.@@@@@@@@@@@@@@@@@@@@@@@@@@@@%.        ..........  .
-// ..... .........:@@@@+@@@@@@@@@-.@@@@@@@@@@******************.    ...   ........ ..  .
-// .. ............@@@@@@@@@@@@@@@-.@@@@@@@@@#    .       .    ..      ..............   .
-// . ............-@@@@@@@@@@@@-....@@@@@@@@@#                           .............  ..  .
-// . ....... ......*@@@@@@@@-... ..@@@@@@@@@#                          ............ .  ..
-// . ..........   .@@@@@@@@.. .....@@@@@@@@@@###%%%%%%%%%%%%..       ............. ..  .
-// ..  .......+@@@@@@@@@@@:   .....@@@@@@@@@@@@@@@@@@@@@@@@@:.    ..................   ..
-// . ........ +@@@@@@@@@@@.    ....@@@@@@@@@@@@@@@@@@@@@@@@@:   . .................. . .
-// ...........*@@@@@@@@@@@:     ...@@@@@@@@@@@@@@@@@@@@@@@@@:    ..................  . .
-// ....... .......=@@@@@@@@. .. . .@@@@@@@@@#    ...............  ..................   ..    .
-// ..... ..........@@@@@@@@%.      @@@@@@@@@#.          ......   ..................  .
-// .    .....    .-@@@@@@@@@@*.    @@@@@@@@@#                   ... .   ..........  .  .
-// .............:@@@@@@@@@@@@@@@*. @@@@@@@@@#                 ..         ...........   .    
-// ..... .  ......*@@@@@@@@@@@@@@-.@@@@@@@@@@%@@@@@%%@@%%%%@@@@@@=        ..........   .
-// ..... . ........:=....*@@@@@@@-.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.          ....... ..
-// .......       .........=@@@@@@-.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=.         ......... .
-// ... . .          ...  .@@@@@@@..%@@@@@@@@@@@@@@@@@@@@@@@@@@@@.      .  . .......    .
-// .........            .....-@%..................................      .   ........   .. .
-// ..............      . . ......................................  .......  .......    . ..
+// ....... ...... ........ ....................................................
+// . ... . ..
+// ............................:.. ............................... ... .
+// ......... .
+// . .... ................#@@@@@*..@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@: . ... . ...
+// ..
+// ..... ................ *@@@@@@-.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@= ... ....
+// ................... ..*@@@@@@-.@@@@@@@@@@@@@@@@@@@@@@@@@@@@%. .......... .
+// ..... .........:@@@@+@@@@@@@@@-.@@@@@@@@@@******************. ... ........ ..
+// .
+// .. ............@@@@@@@@@@@@@@@-.@@@@@@@@@# . . .. .............. .
+// . ............-@@@@@@@@@@@@-....@@@@@@@@@# ............. .. .
+// . ....... ......*@@@@@@@@-... ..@@@@@@@@@# ............ . ..
+// . .......... .@@@@@@@@.. .....@@@@@@@@@@###%%%%%%%%%%%%.. ............. .. .
+// .. .......+@@@@@@@@@@@: .....@@@@@@@@@@@@@@@@@@@@@@@@@:. ..................
+// ..
+// . ........ +@@@@@@@@@@@. ....@@@@@@@@@@@@@@@@@@@@@@@@@: . ..................
+// . .
+// ...........*@@@@@@@@@@@: ...@@@@@@@@@@@@@@@@@@@@@@@@@: .................. . .
+// ....... .......=@@@@@@@@. .. . .@@@@@@@@@# ............... ..................
+// .. .
+// ..... ..........@@@@@@@@%. @@@@@@@@@#. ...... .................. .
+// . ..... .-@@@@@@@@@@*. @@@@@@@@@# ... . .......... . .
+// .............:@@@@@@@@@@@@@@@*. @@@@@@@@@# .. ........... .
+// ..... . ......*@@@@@@@@@@@@@@-.@@@@@@@@@@%@@@@@%%@@%%%%@@@@@@= .......... .
+// ..... . ........:=....*@@@@@@@-.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@. ....... ..
+// ....... .........=@@@@@@-.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=. ......... .
+// ... . . ... .@@@@@@@..%@@@@@@@@@@@@@@@@@@@@@@@@@@@@. . . ....... .
+// ......... .....-@%.................................. . ........ .. .
+// .............. . . ...................................... ....... ....... .
+// ..
